e3bbe7b8bf6af0b11ec7cb368f12f3bdcb6085b9
Søren Fritzbøger <Bente@Srens-MacBook-Pro.local>
2016-05-06 12:30:29 +0200

    Added test

--- 14_CDIO3/src/cdio/test/operator/Operator.java
+++ 14_CDIO3/src/cdio/test/operator/Operator.java
@@ -3,6 +3,7 @@ package cdio.test.operator;
 import static org.junit.Assert.*;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -13,39 +14,59 @@ import cdio.dal.dao.MySQLOperatoerDAO;
 import cdio.dal.dto.OperatoerDTO;
 import cdio.dal.exception.DALException;
 
-public class Operator
-{
+public class Operator {
 	Connector db;
 	
 	@Before
-	public void setup()
-	{
-		try
-		{
+	public void setup() {
+		try {
 			db = new Connector(Constant.server, Constant.port, Constant.database, Constant.username, Constant.password);
-		}
-		catch (InstantiationException | IllegalAccessException | ClassNotFoundException | SQLException e)
-		{
+		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException | SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Test
-	public void GetOperator()
-	{
+	public void GetOperator() {
 		MySQLOperatoerDAO conn = new MySQLOperatoerDAO();
-		
-		try
-		{
+
+		try {
 			OperatoerDTO op = conn.getOperatoer(1);
 			assertNotNull(op);
-		}
-		catch (DALException e)
-		{
+		} catch (DALException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
+	@Test
+	public void GetOperatorList() {
+
+		MySQLOperatoerDAO conn = new MySQLOperatoerDAO();
+		try {
+			List<OperatoerDTO> op = conn.getOperatoerList();
+			assertNotNull(op);
+
+		} catch (DALException e) {
+
+			e.printStackTrace();
+		}
+
+	}
+
+	@Test
+	public void CreateOprator() {
+		OperatoerDTO x = new OperatoerDTO(1234, "bob", "b", "2123213", "000000");
+		MySQLOperatoerDAO conn = new MySQLOperatoerDAO();
+		try {
+			conn.createOperatoer(x);
+			assertNotNull(conn.getOperatoer(1234));
+		} catch (DALException e) {
+
+			e.printStackTrace();
+		}
+
+	}
+
 }
