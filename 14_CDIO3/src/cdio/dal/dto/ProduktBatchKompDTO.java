4eda15c9ef5285dda749de3f474656271ad38c8b
Sara Nordberg <saranordberg@me.com>
2016-06-10 11:42:15 +0200

    changed oprID to user_id

--- 14_CDIO3/src/cdio/dal/dto/ProduktBatchKompDTO.java
+++ 14_CDIO3/src/cdio/dal/dto/ProduktBatchKompDTO.java
@@ -8,19 +8,19 @@ public class ProduktBatchKompDTO implements Serializable
 	public int rbId;        // i omraadet 1-99999999
 	public double tara;
 	public double netto;
-	public int oprId;					// operatoer-nummer
+	public int user_id;					// user-nummer
 
 	public ProduktBatchKompDTO() { }
-	public ProduktBatchKompDTO(int pbId, int rbId, double tara, double netto, int oprId)
+	public ProduktBatchKompDTO(int pbId, int rbId, double tara, double netto, int user_id)
 	{
 		this.pbId = pbId;
 		this.rbId = rbId;
 		this.tara = tara;
 		this.netto = netto;
-		this.oprId = oprId;
+		this.user_id = user_id;
 	}
 	
 	public String toString() { 
-		return pbId + "\t" + rbId +"\t" + tara +"\t" + netto + "\t" + oprId ; 
+		return pbId + "\t" + rbId +"\t" + tara +"\t" + netto + "\t" + user_id ; 
 	}
 }
