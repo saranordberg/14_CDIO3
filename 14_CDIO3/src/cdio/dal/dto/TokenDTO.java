d3670eb4187afa0b43653c2d22f08f01a1bf0c25
Sara Nordberg <saranordberg@me.com>
2016-06-10 11:34:57 +0200

    oprettet token DTO

---
+++ 14_CDIO3/src/cdio/dal/dto/TokenDTO.java
@@ -0,0 +1,25 @@
+package cdio.dal.dto;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+public class TokenDTO implements Serializable 
+{
+	public int user_id;
+	public String token;
+	public Timestamp expiration;
+	
+	public TokenDTO() { }
+	public TokenDTO(int user_id, String token, Timestamp expiration)
+	{
+		this.user_id = user_id;
+		this.token = token;
+		this.expiration = expiration;
+		
+	}
+	
+	public String toString() { 
+		return user_id + "\t" + token +"\t" + expiration; 
+	}
+}
+	
