74004f437b65f153c94b033222ea865db76bbc29
Rasmus <rasmusgundel@hotmail.dk>
2016-06-13 12:21:15 +0200

    More validators

---
+++ 14_CDIO3/src/cdio/client/validate/LeverandoerValidator.java
@@ -0,0 +1,23 @@
+package cdio.client.validate;
+
+public class LeverandoerValidator extends Validator
+{
+	public LeverandoerValidator(Object[] parameters)
+	{
+		super(parameters);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String validate(String value, String name)
+	{
+		if (value.matches("[^0-9\\!@#£¤$%}{=:;*§½<>,_]+$"))
+			
+		{
+			return null;
+		}
+		else
+		{
+			return name + " kan kun være bogstaver og (!@#£¤$%}{=:;*§½<>,_)";
+		}
+	}
+}
