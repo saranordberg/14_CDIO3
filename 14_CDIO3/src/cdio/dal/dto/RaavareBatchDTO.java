56f62f3753a2292e2acfdd675ee769571b5975df
Rasmus <rasmusgundel@hotmail.dk>
2016-06-15 12:21:48 +0200

    no message

--- 14_CDIO3/src/cdio/dal/dto/RaavareBatchDTO.java
+++ 14_CDIO3/src/cdio/dal/dto/RaavareBatchDTO.java
@@ -7,20 +7,22 @@ public class RaavareBatchDTO implements Serializable
 	public int rbId; // i omraadet 1-99999999
 	public int raavareId; // i omraadet 1-99999999
 	public double maengde; // kan vaere negativ
+	public String raavareNavn;
 	
 	public RaavareBatchDTO()
 	{
 	}
 	
-	public RaavareBatchDTO(int rbId, int raavareId, double maengde)
+	public RaavareBatchDTO(int rbId, int raavareId, double maengde, String raavareNavn)
 	{
 		this.rbId = rbId;
 		this.raavareId = raavareId;
 		this.maengde = maengde;
+		this.raavareNavn = raavareNavn;
 	}
 	
 	public String toString()
 	{
-		return rbId + "\t" + raavareId + "\t" + maengde;
+		return rbId + "\t" + raavareId + "\t" + maengde + "\t" + raavareNavn;
 	}
 }
